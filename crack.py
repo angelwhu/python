010f27e0c55ff2a87288f3f07d3bc1dc08e61d7e
angelwhu <huanqiwhu@163.com>
2017-03-12 19:19:57 +0800

    爆破md5脚本

---
+++ crack.py
@@ -0,0 +1,26 @@
+import sys
+import crypt, getpass, pwd
+import string
+import hashlib
+
+def md5(src):
+    m2 = hashlib.md5()
+    m2.update(src)
+    return m2.hexdigest()
+
+def crack(deep,pw):
+    if deep == 0:
+        return;
+    for c in string.printable:
+        crack(deep-1,pw + c)
+
+        passwd  = pw + c
+        crypted = md5(passwd)
+        if crypted[0:6] == sys.argv[1]:
+            print passwd
+            print crypted
+            print crypted[0:6]
+            print "success"
+            exit()
+if __name__ == '__main__':
+    crack(4,"")
